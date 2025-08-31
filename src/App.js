dfe6fed65d5dfe43f729d330bc9ee1a543696741
Simple002 <meyrzhan04.04@mail.ru>
2025-08-31 23:38:34 +0500

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_KEY}/api/data`)
+      .then((res) => res.json())
+      .then((json) => setData(json.message));
+  }, []);
+
+  return <div>{data}</div>;
 }
 
 export default App;
